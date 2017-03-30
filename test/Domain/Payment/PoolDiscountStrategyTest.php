1b3601fb432c70498b5a738e1464e2cd010bf9c6
Łukasz Wróbel <lwrobel@users.noreply.github.com>
2017-03-30 17:21:01 +0200

    Iteration 1: test name provided

---
+++ test/Domain/Payment/PoolDiscountStrategyTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace RstGroup\ConferenceSystem\Domain\Payment\Test;
+
+class PoolDiscountStrategyTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function returns_discount_equal_to_zero_if_discount_pool_is_empty()
+    {
+    }
+}
