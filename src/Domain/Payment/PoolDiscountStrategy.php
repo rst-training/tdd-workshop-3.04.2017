376695226467107385439809e09f5513206d09c1
Łukasz Wróbel <lwrobel@users.noreply.github.com>
2017-03-30 17:32:24 +0200

    Iteration 1: test provided

---
+++ src/Domain/Payment/PoolDiscountStrategy.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace RstGroup\ConferenceSystem\Domain\Payment;
+
+
+use RstGroup\ConferenceSystem\Domain\Reservation\ConferenceId;
+use RstGroup\ConferenceSystem\Domain\Reservation\Seat;
+
+class PoolDiscountStrategy implements SeatDiscountStrategy
+{
+    private $conferenceId;
+
+    /**
+     * @param ConferenceId $conferenceId
+     */
+    public function __construct(ConferenceId $conferenceId)
+    {
+        $this->conferenceId = $conferenceId;
+    }
+
+    /**
+     * @param Seat $seat
+     * @return int discount
+     */
+    public function calculate(Seat $seat)
+    {
+    }
+}
--- test/Domain/Payment/PoolDiscountStrategyTest.php
+++ test/Domain/Payment/PoolDiscountStrategyTest.php
@@ -2,6 +2,12 @@
 
 namespace RstGroup\ConferenceSystem\Domain\Payment\Test;
 
+use RstGroup\ConferenceSystem\Domain\Payment\Currency;
+use RstGroup\ConferenceSystem\Domain\Payment\Money;
+use RstGroup\ConferenceSystem\Domain\Payment\PoolDiscountStrategy;
+use RstGroup\ConferenceSystem\Domain\Reservation\ConferenceId;
+use RstGroup\ConferenceSystem\Domain\Reservation\Seat;
+
 class PoolDiscountStrategyTest extends \PHPUnit_Framework_TestCase
 {
     /**
@@ -9,5 +15,14 @@ class PoolDiscountStrategyTest extends \PHPUnit_Framework_TestCase
      */
     public function returns_discount_equal_to_zero_if_discount_pool_is_empty()
     {
+        $seatQuantity = 50;
+        $conferenceId = new ConferenceId(7);
+        $poolDiscountStrategy = new PoolDiscountStrategy($conferenceId);
+        $seat = new Seat("Regular", $seatQuantity);
+
+        $discount = $poolDiscountStrategy->calculate($seat);
+
+        $expectedDiscount = 0;
+        $this->assertSame($expectedDiscount, $discount);
     }
 }
